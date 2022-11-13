ec6a2956f1dc9c148311e18cd8da5fc3c51d5d07
Lavialee <50073365+Lavialee@users.noreply.github.com>
2022-11-13 23:02:31 +0100

    baby animation

--- Main.gd
+++ Main.gd
@@ -10,7 +10,11 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-
+#if baby type = thing, despawn, make new baby, add score	
+#if not ubrat na timeru za trest
+func _on_Baby_saved():
+	score += 1
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
