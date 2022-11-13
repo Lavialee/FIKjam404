1251f8d10959bdca13636e5a065877d1af35b84d
Lavialee <50073365+Lavialee@users.noreply.github.com>
2022-11-13 21:41:19 +0100

    aa

--- Main.gd
+++ Main.gd
@@ -1,48 +1,17 @@
-extends Node
+extends Node2D
 
-export(PackedScene) var mob_scene
-var score
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize()
-	new_game()
+	pass # Replace with function body.
 
-func game_over():
-	$ScoreTimer.stop()
-	$MobTimer.stop()
 
-func new_game():
-	score = 0
-	$Player.start($StartPosition.position)
-	$StartTimer.start()
-	
-	
-func _on_MobTimer_timeout():
-	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instance()
-
-	# Choose a random location on Path2D.
-	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
-	mob_spawn_location.offset = randi()
-
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
-
-	# Set the mob's position to a random location.
-	mob.position = mob_spawn_location.position
-
-	# Choose the velocity for the mob.
-	var velocity = Vector2(rand_range(150.0, 250.0), 5.0)
-	mob.linear_velocity = velocity.rotated(direction)
-
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
-	
-	
-func _on_ScoreTimer_timeout():
-	score += 1
-
-func _on_StartTimer_timeout():
-	$MobTimer.start()
-	$ScoreTimer.start()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
 
