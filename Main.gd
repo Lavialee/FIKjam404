e09bf89e7e6caac649dfd70dc598234ced5a2632
Lavialee <50073365+Lavialee@users.noreply.github.com>
2022-11-13 23:50:33 +0100

    ajsdhjsha

--- Main.gd
+++ Main.gd
@@ -1,21 +1,16 @@
-extends Node2D
+extends Node
 
+export(PackedScene) var mob_scene
+var score
+
+func _ready():
+	randomize()
+	
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-#if baby type = thing, despawn, make new baby, add score	
-#if not ubrat na timeru za trest
-func _on_Baby_saved():
-	score += 1
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
--- baby.gd
+++ baby.gd
@@ -6,11 +6,11 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize()
-
-
-
-
+	$AnimatedSprite.playing = true
+	var baby_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = baby_types[randi() % baby_types.size()]
+	print(baby_types)
+	print(baby_types[randi() % baby_types.size()])
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
