688621ea435b5d6bac570d135021845ef4da6f1e
Lavialee <50073365+Lavialee@users.noreply.github.com>
2022-11-13 16:58:18 +0100

    hud remove

--- Player.gd
+++ Player.gd
@@ -7,7 +7,6 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 	
-
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector - aka defaultne se nehybe
 	if Input.is_action_pressed("move_right"):
@@ -21,14 +20,12 @@ func _process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 
-
 	position += velocity * delta #delta je frame length -> chceme aby movement byl smooth
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
 
 func _on_Player_body_entered(_body):
-	hide() # Player disappears after being hit.
 	emit_signal("hit")
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
